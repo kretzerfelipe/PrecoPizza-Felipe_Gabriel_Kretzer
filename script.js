769116da77a84d811af114f3b297159ec5513d47
Felipe Gabriel Kretzer <felipegk2006@gmail.com>
2023-11-04 15:16:43 -0300

    v4 [Reformatei o cógigo com os conteúdos das aulas recentes + estilização + recurso de % + correção de possíveis bugs]

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //Função para mudança de value circular
 
-document.querySelector("#idDivLado2").style.display = "none"
+document.querySelector("#idLado2").style.display = "none"
+document.querySelector("#idLado2Label").style.display = "none"
 
 var mudarValueCircular = document.querySelector("#idCircular")
 
 mudarValueCircular.addEventListener("click", tratamentoMudarValueCircular)
 
 function tratamentoMudarValueCircular() {
-    document.querySelector("#idDivLado2").style.display = "none"
+    document.querySelector("#idLado2").style.display = "none"
+    document.querySelector("#idLado2Label").style.display = "none"
     document.querySelector("label[for='idDiametroOuLado1']").textContent = "Diametro"
 }
 
@@ -17,24 +19,14 @@ var mudarValueQuadrada = document.querySelector("#idQuadrada")
 mudarValueQuadrada.addEventListener("click", tratamentoMudarValueQuadrada)
 
 function tratamentoMudarValueQuadrada() {
-    document.querySelector("#idDivLado2").style.display = "block"
+    document.querySelector("#idLado2").style.display = "block"
+    document.querySelector("#idLado2Label").style.display = "block"
     document.querySelector("label[for='idDiametroOuLado1']").textContent = "Lado 1"
 }
 
 
-//Array pizzas circulares
-
-const nomesComerciaisCirculares = []
-const diametrosCirculares = []
-const preçosCirculares = []
-
-
-//Array pizzas quadradas
-const nomesComerciaisQuadradas = []
-const lados1Quadradas = []
-const lados2Quadradas = []
-const preçosQuadradas = []
-
+//Array pizzas 
+const pizzas = []
 
 // Função para colocar os intens em arrays
 var enviarValores = document.querySelector("#idBotãoEnviar")
@@ -45,6 +37,7 @@ function tratamentoEnviarValores() {
     let quadrada = document.querySelector("#idQuadrada").checked
 
     if (circular) {
+        // Declarando as variáveis
         let nomeComercial = document.querySelector("#idNomeComercial").value
         nomeComercial = nomeComercial.toUpperCase()
 
@@ -52,32 +45,49 @@ function tratamentoEnviarValores() {
 
         let preço = Number(document.querySelector("#idPreço").value)
 
+        let raio = diametro / 2
+        area = Math.PI * (raio * raio)
+
+        let custoBenefício = preço / area
+
         let valorInserido = false
 
-        if (nomesComerciaisCirculares.length == 0) {
-            nomesComerciaisCirculares.push(nomeComercial)
-            diametrosCirculares.push(diametro)
-            preçosCirculares.push(document.querySelector("#idPreço").value)
-        } else if (nomesComerciaisCirculares.length != 0) {
-            for (let i = 0; i < nomesComerciaisCirculares.length; i++) {
-                if (nomesComerciaisCirculares[i] == nomeComercial) {
-                    valorInserido = true
-                } else if (diametrosCirculares[i] == diametro) {
+        if (diametro == "" || preço == "" || nomeComercial == '') {
+            alert("Insira todos os dados")
+        } else if (diametro >= 1000) {
+            alert("O tamanho da pizza é grande demais")
+        } else if (pizzas.length == 0) {
+            let pizza = {
+                nomeComercial: nomeComercial,
+                diametro: diametro,
+                preço: preço,
+                area: area,
+                custoBenefício: custoBenefício,
+                medidas: diametro,
+                custoBenefícioPorcentagem: ""
+            }
+            pizzas.push(pizza)
+        } else if (pizzas.length != 0) {
+            pizzas.forEach(element => {
+                if (element.nomeComercial == nomeComercial) {
                     valorInserido = true
                 }
-            }
+            });
             if (valorInserido) {
-                alert("Este nome/tamanho de pizza já foi inserido")
+                alert("Este nome de pizza já foi inserido")
             } else {
-                nomesComerciaisCirculares.push(nomeComercial)
-                diametrosCirculares.push(diametro)
-                preçosCirculares.push(document.querySelector("#idPreço").value)
+                let pizza = {
+                    nomeComercial: nomeComercial,
+                    diametro: diametro,
+                    preço: preço,
+                    area: area,
+                    custoBenefício: custoBenefício,
+                    medidas: diametro,
+                    custoBenefícioPorcentagem: ""
+                }
+                pizzas.push(pizza)
             }
         }
-        document.querySelector("#idNomeComercial").value = ""
-        document.querySelector("#idDiametroOuLado1").value = ""
-        document.querySelector("#idLado2").value = ""
-        document.querySelector("#idPreço").value = ""
     } else if (quadrada) {
         let nomeComercial = document.querySelector("#idNomeComercial").value
         nomeComercial = nomeComercial.toUpperCase()
@@ -88,125 +98,109 @@ function tratamentoEnviarValores() {
 
         let preço = Number(document.querySelector("#idPreço").value)
 
+        let area = lado1 * lado2
+
+        let custoBenefício = preço / area
+
         let valorInserido = false
 
-        if (nomesComerciaisQuadradas.length == 0) {
-            nomesComerciaisQuadradas.push(nomeComercial)
-            lados1Quadradas.push(lado1)
-            lados2Quadradas.push(lado2)
-            preçosQuadradas.push(document.querySelector("#idPreço").value)
-        } else if (nomesComerciaisQuadradas.length != 0) {
-            for (let i = 0; i < nomesComerciaisQuadradas.length; i++) {
-                if (nomesComerciaisQuadradas[i] == nomeComercial) {
-                    valorInserido = true
-                } else if (lados1Quadradas[i] && lados2Quadradas[i] == diametro) {
+        if (lado1 == "" || lado2 == "" || preço == "" || nomeComercial == '') {
+            alert("Insira todos os dados")
+        } else if (lado1 >= 1000 || lado2 >= 1000) {
+            alert("O tamanho da pizza é grande demais")
+        } else if (pizzas.length == 0 && pizzas.length == 0) {
+            let pizza = {
+                nomeComercial: nomeComercial,
+                lado1: lado1,
+                lado2: lado2,
+                preço: preço,
+                area: area,
+                custoBenefício: custoBenefício,
+                medidas: lado1 + " x " + lado2,
+                custoBenefícioPorcentagem: ""
+            }
+            pizzas.push(pizza)
+        } else if (pizzas.length != 0 || pizzas.length != 0) {
+            pizzas.forEach(element => {
+                if (element.nomeComercial == nomeComercial) {
                     valorInserido = true
                 }
-            }
+            });
             if (valorInserido) {
-                alert("Este nome/tamanho de pizza já foi inserido")
+                alert("Este nome de pizza já foi inserido")
             } else {
-                nomesComerciaisQuadradas.push(nomeComercial)
-                lados1Quadradas.push(diametro)
-                lados2Quadradas.push(lado2)
-                preçosQuadradas.push(document.querySelector("#idPreço").value)
+                let pizza = {
+                    nomeComercial: nomeComercial,
+                    lado1: lado1,
+                    lado2: lado2,
+                    preço: preço,
+                    area: area,
+                    custoBenefício: custoBenefício,
+                    medidas: lado1 + "x" + lado2,
+                    custoBenefícioPorcentagem: ""
+                }
+                pizzas.push(pizza)
             }
         }
-        document.querySelector("#idNomeComercial").value = ""
-        document.querySelector("#idDiametroOuLado1").value = ""
-        document.querySelector("#idLado2").value = ""
-        document.querySelector("#idPreço").value = ""
     } else {
         alert("Você deve escolher um tipo de pizza")
     }
+    document.querySelector("#idNomeComercial").value = ""
+    document.querySelector("#idDiametroOuLado1").value = ""
+    document.querySelector("#idLado2").value = ""
+    document.querySelector("#idPreço").value = ""
 
+    // Função para ordenar os custos benefícios
+    pizzas.sort(function (a,b) {
+        if (a.custoBenefício > b.custoBenefício) {
+            return 1
+        }
+        if (a.custoBenefício < b.custoBenefício) {
+            return -1
+        }
+        return 0
+    })
 
+    pizzas[0].custoBenefícioPorcentagem = "Melhor Custo-Benefício"
 
-// Função para colocar os intens em arrays principais e ordená-los
-    const areasCirculares = []
-    for (let i = 0; i < diametrosCirculares.length; i++) {
-        let raio = diametrosCirculares[i] / 2
-        areasCirculares[i] = Math.PI * (raio * raio)
+    for (let i = 1; i < pizzas.length; i++) {
+        pizzas[i].custoBenefícioPorcentagem = (pizzas[i].custoBenefício - pizzas[0].custoBenefício) / pizzas[0].custoBenefício * 100
+        pizzas[i].custoBenefícioPorcentagem = pizzas[i].custoBenefícioPorcentagem.toFixed(2) + "% mais cara."
     }
-    console.log(areasCirculares)
-
-    const areasQuadradas = []
-    for (let i = 0; i < lados1Quadradas.length; i++) {
-        areasQuadradas[i] = lados1Quadradas[i] * lados2Quadradas[i]
-    }
-
-    const custoBenefícioCirculares = []
-    for (let i = 0; i < areasCirculares.length; i++) {
-        custoBenefícioCirculares[i] = preçosCirculares[i] / areasCirculares[i]
-    }
-
-    const custoBenefícioQuadradas = []
-    for (let i = 0; i < areasQuadradas.length; i++) {
-        custoBenefícioQuadradas[i] = preçosQuadradas[i] / areasQuadradas[i]
-    }
-
-    const custoBenefícioTotal = []
-    const nomesComerciaisTotal = []
-    const medidasTotal = []
-    const preçosTotal = []
-
-    for (let i = 0; i < custoBenefícioCirculares.length; i++) {
-        custoBenefícioTotal.push(custoBenefícioCirculares[i])
-        nomesComerciaisTotal.push(nomesComerciaisCirculares[i])
-        medidasTotal.push(diametrosCirculares[i])
-        preçosTotal.push(preçosCirculares[i])
-    }
-
-    for (let i = 0; i < custoBenefícioQuadradas.length; i++) {
-        custoBenefícioTotal.push(custoBenefícioQuadradas[i])
-        nomesComerciaisTotal.push(nomesComerciaisQuadradas[i])
-        medidasTotal.push(lados1Quadradas[i] + ", " + lados2Quadradas)
-        preçosTotal.push(preçosQuadradas[i])
-    }
-
-
-    const custoBenefícioCrescente = []
-    const nomesComerciaisCrescente = []
-    const medidasCrescente = []
-    const preçosCrescente = []
-
-    for (let i = 0; i < custoBenefícioTotal.length; i++) {
-        let contagem = -1
-        for (let a = 0; a < custoBenefícioTotal.length; a++)
-            if (custoBenefícioTotal[i] >= custoBenefícioTotal[a]) {
-                contagem++
-            }
-        custoBenefícioCrescente[contagem] = custoBenefícioTotal[i]
-        nomesComerciaisCrescente[contagem] = nomesComerciaisTotal[i]
-        medidasCrescente[contagem] = medidasTotal[i]
-        preçosCrescente[contagem] = preçosTotal[i]
-    }
-    console.log(custoBenefícioCrescente)
 
     // Colocar em uma tabela
     let tbody = document.querySelector("#idTbody")
     tbody.innerHTML = ""
-    for (let i = 0; i < custoBenefícioCrescente.length; i++) {
-        let nomeTD = nomesComerciaisCrescente[i]
-        let tamanhoTD = medidasCrescente[i]
-        let preçoTD = preçosCrescente[i]
-        let custoBenefícioTD = custoBenefícioCrescente[i]
+
+    pizzas.forEach(element => {
+        let nomeTD = element.nomeComercial
+        let areaTD = element.area
+        let tamanhoTD = element.medidas        
+        let preçoTD = element.preço
+        let custoBenefícioTD = element.custoBenefício
+        let custoBenefícioPorcentagemTD = element.custoBenefícioPorcentagem
 
         let tr = document.createElement("tr")
         let td1 = document.createElement("td")
         let td2 = document.createElement("td")
         let td3 = document.createElement("td")
         let td4 = document.createElement("td")
+        let td5 = document.createElement("td")
+        let td6 = document.createElement("td")
 
         td1.innerHTML = nomeTD
-        td2.innerHTML = tamanhoTD
-        td3.innerHTML = preçoTD
-        td4.innerHTML = custoBenefícioTD
+        td2.innerHTML = areaTD.toFixed(2)
+        td3.innerHTML = tamanhoTD
+        td4.innerHTML = preçoTD
+        td5.innerHTML = custoBenefícioTD.toFixed(5)
+        td6.innerHTML = custoBenefícioPorcentagemTD
 
         tbody.appendChild(tr)
         tr.appendChild(td1)
         tr.appendChild(td2)
         tr.appendChild(td3)
         tr.appendChild(td4)
-    }
+        tr.appendChild(td5)
+        tr.appendChild(td6)
+    });
 }
